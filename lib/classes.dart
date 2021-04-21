fe9ec06af3c717b8dca419cfe226478faa82fa2c
QuentinTedeschi <quentin.tedeschi@gmail.com>
2021-04-21 16:02:52 +0200

    Init classes

---
+++ lib/classes.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+class Course {
+  String name;
+  int repartition;
+  int prog_exo;
+  int prog_theory;
+  int interest;
+  Course(this.name, this.repartition, this.prog_exo, this.prog_theory, this.interest) {
+    // Code if needed.
+  }
+}
+
+class Block {
+  Course course;
+  int length;
+  bool theory;
+  Block(this.course, this.length, this.theory) {
+    // Code if needed.
+  }
+}
+
+class Day {
+  List<Block> blocks;
+  DateTime date;
+  TimeOfDay start;
+  TimeOfDay finish;
+  Day(this.blocks, this.date, this.start, this.finish) {
+    // Code if needed.
+  }
+}
+
+class Calendar {
+  List<Day> days;
+  Calendar(this.days) {
+    // Code if needed.
+  }
+}
+
+class User {
+  String name;
+  List<Calendar> calendars;
+  String passwordhash;
+  List<Course> priority;
+  List<Course> favorite;
+
+  User(this.name, this.calendars, this.passwordhash, this.priority, this.favorite) {
+    // Code if needed.
+  }
+}
