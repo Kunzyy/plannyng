b02d4eb3c62a46dcead9e152f24aa41b9a94adea
QuentinTedeschi <quentin.tedeschi@gmail.com>
2021-05-01 22:48:49 +0200

    Update class + assignation

--- lib/classes.dart
+++ lib/classes.dart
@@ -14,9 +14,10 @@ class Course {
 
 class Block {
   Course course;
-  int length;
+  TimeOfDay start;
+  TimeOfDay finish;
   bool theory;
-  Block(this.course, this.length, this.theory) {
+  Block(this.course, this.start, this.finish, this.theory) {
     // Code if needed.
   }
 }
@@ -24,17 +25,20 @@ class Block {
 class Day {
   List<Block> blocks;
   DateTime date;
-  TimeOfDay start;
-  TimeOfDay finish;
-  Day(this.blocks, this.date, this.start, this.finish) {
+  Day(this.blocks, this.date) {
     // Code if needed.
   }
 }
 
 class Calendar {
   List<Day> days;
-  Calendar(this.days) {
-    // Code if needed.
+  Calendar(DateTime begin, DateTime end) {
+    int difference = end.difference(begin).inDays;
+    for(var i; i <= difference; i++){
+      DateTime tmpdate = begin.add(Duration(days: i));
+      Day tmpday = Day([], tmpdate);
+      days.add(tmpday);
+    }
   }
 }
 
@@ -47,26 +51,5 @@ class User {
 
   User(this.name, this.calendars, this.passwordhash, this.courses) {
     // Code if needed.
-  }
-
-
-  //Modélisation aka temporaire
-  void createCalendar(){
-    var remainingCourse = new Map();
-    this.courses.forEach((cours) {
-      remainingCourse[cours] = [cours.prog_exo, cours.prog_theory];
-    });
-    this.calendars[0].days.forEach((day) {
-      //on crée les blocs
-      //on assigne le bloc qui commence le plus proche de l'heure de midiPause à la pause de midi
-      day.blocks.forEach((bloc) {
-        //fill it
-        //Si avant la pause de midi, cours avec grand interet, sinon bas
-        //remainingCourse[cours ajouté][0] -= x pour les exo
-        //remainingCourse[cours ajouté][0] -= x pour la théorie
-        //quand tout est = 0 à c'est fini
-      });
-      //On check si tout est fini et si c'est pas le cas alors le planning n'est pas possible à faire
-    });
   }
 }
