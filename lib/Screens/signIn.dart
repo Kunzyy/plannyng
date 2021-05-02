ce27fdfe299d19cc43768df2f945f31bea64764b
MargotLiagre <171245@umons.ac.be>
2021-05-02 10:52:04 +0200

    Login and SignIn first version

--- lib/Screens/signIn.dart
+++ lib/Screens/signIn.dart
@@ -2,15 +2,100 @@ import 'package:flutter/material.dart';
 
 import '../Constants.dart';
 
+import 'login.dart';
+import 'home.dart';
+
 class SignIn extends StatelessWidget {
+  final _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+          title: Text("Inscription"),
+          backgroundColor: primaryColor,
+          elevation: 0.0),
       body: Container(
         constraints: BoxConstraints.expand(),
         decoration: backgroundDeco,
-
-
+        child: Column(
+          children: <Widget>[
+            Form(
+              key: _formKey,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  TextFormField(
+                    decoration: const InputDecoration(
+                      hintText: formMail,
+                    ),
+                    validator: (value) {
+                      if (value.isEmpty) {
+                        return formErrorMail;
+                      }
+                      return null;
+                    },
+                  ),
+                  TextFormField(
+                    decoration: const InputDecoration(
+                      hintText: formMDP,
+                    ),
+                    validator: (value) {
+                      if (value.isEmpty) {
+                        return formErrorMDP;
+                      }
+                      return null;
+                    },
+                  ),
+                  TextFormField(
+                    decoration: const InputDecoration(
+                      hintText: formValMDP,
+                    ),
+                    validator: (value) {
+                      if (value.isEmpty) {
+                        return formErrorValMDP;
+                      }
+                      return null;
+                    },
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 16.0),
+                    child: ElevatedButton(
+                      onPressed: () {
+                        // Validate will return true if the form is valid, or false if
+                        // the form is invalid.
+                        if (_formKey.currentState.validate()) {
+                          ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                            // Process data.
+                              content: Text(
+                                  "Traitement des données")));
+                          dynamic newRoute = MaterialPageRoute(builder: (context) => Home());
+                          Navigator.pushReplacement(context, newRoute);
+                        }
+                      },
+                      child: Text("M'inscrire !"),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              child: Column(
+                children: <Widget>[
+                  Text("Vous avez déjà un compte ?"),
+                  ElevatedButton(
+                    onPressed: () {
+                      dynamic newRoute =
+                          MaterialPageRoute(builder: (context) => Login());
+                      Navigator.pushReplacement(context, newRoute);
+                    },
+                    child: Text("Je me connecte !"),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
